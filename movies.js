55f7447195c895acf12e3f2aaa1be505a3ec0300
viryisb <44821968+viryisb@users.noreply.github.com>
2019-05-22 16:11:23 -0300

    Add files via upload

---
+++ movies.js
@@ -0,0 +1,15 @@
+if ( categoria === 'toprated'){
+    // url = `` 
+    document.querySelector('#peliculas h2').innerHTML
+}
+
+if ( categoria === 'toprated'){
+    // url = `` 
+    document.querySelector('#peliculas h1').innerHTML
+}
+
+fetch(url)
+.then(res => res.json())
+.then(movies =>{
+
+})
