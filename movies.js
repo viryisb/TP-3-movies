fdb4d0a4523b3b6001de1c5e8f6b0489b5f0d03b
viryisb <44821968+viryisb@users.noreply.github.com>
2019-05-26 19:54:24 -0300

    Add files via upload

--- movies.js
+++ movies.js
@@ -1,18 +1,22 @@
 console.log("hola mundo esto anda?")
+/* const api_key="5183000ac12d7f78e1f2353e8405470e" */
 
-
-/* if ( categoria === 'toprated'){
-    // url = `` 
-    document.querySelector('#peliculas h2').innerHTML
-}
-
-if ( categoria === 'toprated'){
-    // url = `` 
-    document.querySelector('#peliculas h1').innerHTML
-}
-
-fetch(url)
-.then(res => res.json())
-.then(movies =>{
-
-})  */
+const traerPelis = categoria=> {
+    fetch('https://api.themoviedb.org/3/movie/popular?api_key=5183000ac12d7f78e1f2353e8405470e')
+    .then(res=>res.json())
+    .then(data=>{
+        console.log (data)
+        /* const ul = document.querySelector('peliculas') */; 
+       for (i=0;i<data.results.length;i++){
+       /*  (data.results[i].title) */;
+         if(i < 5){
+  console.log(data.results[i].title)
+        }
+       }
+    })
+  }
+  
+        
+    
+  
+    traerPelis("popular")
